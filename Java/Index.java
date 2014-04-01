129c0ad4797888d527763f569ce24005eb9d69e0
Aubrey <aubrey.alston.1@gmail.com>
2014-04-01 10:46:33 -0400

    Added index.html; defined test servlet action for form action call.  Added web.xml configuration for Resin server.

---
+++ Java/Index.java
@@ -0,0 +1,25 @@
+/**
+ * Index.java - Splash:  displays index of available forums
+ * plus a link to log in.
+ */
+
+import java.io.*;
+import javax.servlet.http.*;
+import javax.servlet.*;
+import java.sql.*;
+import oracle.jdbc.*;
+import oracle.jdbc.pool.*;
+
+public class Index extends HttpServlet{
+	public void doGet ( HttpServletRequest request,
+						HttpServletResponse response) 
+				throws ServletException, IOException {
+			
+			response.setContentType("text/html");
+			
+			PrintWriter outStream = response.getWriter();
+			
+			outStream.println("Placeholder string.");
+	}
+}
+
